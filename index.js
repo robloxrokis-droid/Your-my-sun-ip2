d8b0e3424724c974ea1b9a6c88cd44febce1226a
robloxrokis-droid <robloxrokis@gmail.com>
2026-04-15 21:34:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require("express");
-const app = express();
-
 app.get("/", (req, res) => {
-  console.log("접속 IP:", req.ip);
-  res.send("환영합니다 😎");
-});
+  const ip =
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress;
 
-app.listen(3000, () => {
-  console.log("서버 실행중");
+  console.log("IP:", ip);
+
+  res.send("OK");
 });

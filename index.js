0e22e4dcff2dd4f0a5e7fed337ce5ee1a0f1d3b4
robloxrokis-droid <robloxrokis@gmail.com>
2026-04-16 18:38:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const app = express();
 
 app.get("/", (req, res) => {
   const ip =
-    req.headers["x-forwarded-for"] ||
+    (req.headers["x-forwarded-for"] || "").split(",")[0] ||
     req.socket.remoteAddress;
 
+  const agent = req.headers["user-agent"];
+
   console.log("IP:", ip);
+  console.log("AGENT:", agent);
+
+  if (agent && agent.includes("UptimeRobot")) {
+    console.log("👉 봇 접속");
+  } else {
+    console.log("🔥 진짜 사람 접속");
+  }
 
   res.send("OK");
 });
